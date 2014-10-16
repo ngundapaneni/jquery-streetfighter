7f2d245e29166f8ea62824ffc29384188c21f510
Natasha <ngundapaneni@gmail.com>
2014-10-16 06:58:44 -0500

    jquery-streetfigther implementation
    Added all the necessary functionality for the street fighter app

---
+++ js/app.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+	$(".ryu").on("mouseenter", function() {
+		$(".ryu-still").hide();
+		$(".ryu-ready").show();
+	})
+	.on("mouseleave", function() {
+		$(".ryu-ready").hide();
+		$(".ryu-still").show();
+	})
+	.on("mousedown", function() {
+		playHadouken();
+
+		$(".ryu-ready").hide();
+		$(".ryu-throwing").show();
+		$(".hadouken").finish().show()
+		.animate(
+  		{'left': '300px'},
+  		500,
+  		function() {
+    		$(this).hide();
+    		$(this).css('left', '-212px');
+  		});
+	})
+	.on("mouseup", function() {
+		$(".ryu-throwing").hide();
+		$(".ryu-ready").show();
+	});
+
+	$(document).on("keydown", function(event) {
+		if (event.which == 88) {
+			$(".ryu-still").hide();
+			$(".ryu-coolpose").show();
+		}
+	}).on("keyup", function(event) {
+		if (event.which == 88) {
+			$(".ryu-coolpose").hide();
+			$(".ryu-still").show();
+		}
+	});
+});
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
